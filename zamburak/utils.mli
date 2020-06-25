08b8a9006f53a17fb52346fdfbc721d5db3437f2
MichaelKonobeev <konobeev.michael@gmail.com>
2020-06-25 00:55:28 -0600

    Remove float_mean, float_std from Zamburak.Utils

--- zamburak/utils.mli
+++ zamburak/utils.mli
@@ -4,8 +4,6 @@ val argmax : 'a array -> int
 module Array : sig
   include module type of Array
 
-  val float_mean : float array -> float
-  val float_std : ?mean:float -> float array -> float
   val accumulate : ('a -> 'a -> 'a) -> 'a array -> 'a array
 end
 
